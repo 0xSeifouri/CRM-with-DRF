a46a08730d4b46b398a03ec9a345877110e1d2af
Shahrooz Seifouri <ssayfouri@gmail.com>
2023-03-30 16:52:20 +0430

    complete write url_tests

--- crm/tests.py
+++ crm/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.shortcuts import reverse
+from django.contrib.auth.models import User
+
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, APIClient, APIRequestFactory
 
 from .models import CustomerData
 
@@ -8,6 +12,10 @@ class TestUrlResponse(TestCase):
     # Url Test Response use name
     @classmethod
     def setUpTestData(cls):
+        cls.factory = APIRequestFactory()
+        cls.user = User.objects.create(username='user1')
+        cls.token = Token.objects.create(user=cls.user)
+        cls.token.save()
         cls.crm = CustomerData.objects.create(
             first_name='test',
             last_name='test',
@@ -23,38 +31,35 @@ class TestUrlResponse(TestCase):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
-    # def test_create_url(self):
-    #     response = self.client.get(reverse('create'))
-    #     self.assertEqual(response.status_code, 200)
+    def test_create_url(self):
+        response = self.client.post(reverse('create'), {'first_name': 'test', 'last_name': 'test',
+                                                        'email': 'test@email.com', 'phone': '09123456789',
+                                                        'address': 'test st', 'city': 'test city',
+                                                        'state': 'test state', 'zipcode': '0123456789'})
+        self.assertEqual(response.status_code, 302)
 
-    # def test_retrieve_url(self):
-    #     response = self.client.get(f'show/{self.crm.id}/', args=[self.crm.id])
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_update_url(self):
-    #     response = self.client.get(reverse('update'))
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_destroy_url(self):
-    #     response = self.client.get(reverse('destroy'))
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_api_list_url(self):
-    #     response = self.client.get(reverse('list'))
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_api_create_url(self):
-    #     response = self.client.get(reverse('api-create'))
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_api_retrieve_url(self):
-    #     response = self.client.get(reverse('api-retrieve'))
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_api_update_url(self):
-    #     response = self.client.get(reverse('api-update'))
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_api_destroy_url(self):
-    #     response = self.client.get(reverse('api-destroy'))
-    #     self.assertEqual(response.status_code, 200)
+    def test_retrieve_url(self):
+        response = self.client.get(reverse('retrieve', args=[self.crm.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_url(self):
+        response = self.client.get(reverse('update', args=[self.crm.id]), {'first_name': 'test', 'last_name': 'test',
+                                                                           'email': 'test@email.com',
+                                                                           'phone': '09123456789',
+                                                                           'address': 'test st', 'city': 'test city',
+                                                                           'state': 'test state',
+                                                                           'zipcode': '0123456789'})
+        self.assertEqual(response.status_code, 302)
+
+    def test_destroy_url(self):
+        response = self.client.get(reverse('destroy', args=[self.crm.id]))
+        self.assertEqual(response.status_code, 302)
+
+    def test_api_list_url(self):
+        response = self.client.get(reverse('list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_retrieve_url(self):
+        response = self.client.get(reverse('api-retrieve', args=[self.crm.id]))
+        self.assertEqual(response.status_code, 200)
+
